428c842ad434203fe0a8526d4882f3e373ab018f
Alexander Romanov <aromanov07@gmail.com>
2025-03-30 19:13:34 +0300

    Fix package

--- Package.swift
+++ Package.swift
@@ -19,12 +19,7 @@ let localDependencies: [PackageDescription.Package.Dependency] = commonDependenc
     .package(name: "OversizeModels", path: "../OversizeModels"),
 ]
 
-
-var dependencies: [PackageDescription.Package.Dependency] = remoteDependencies
-
-if ProcessInfo.processInfo.environment["BUILD_MODE"] == "DEV" {
-    dependencies = localDependencies
-}
+let dependencies: [PackageDescription.Package.Dependency] = localDependencies
 
 let package = Package(
     name: "OversizeNetwork",
